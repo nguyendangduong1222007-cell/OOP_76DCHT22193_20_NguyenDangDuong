a43196aa636c5c409023deb1c94b8390fe155e5a
76DCHT22193_20_NguyenDangDuong <nguyendangduong1222007@gmail.com>
2026-08-19 20:40:53 +0700

    BT01-CP2: Hoan thanh ham nhap xuat

--- BT01.cpp
+++ BT01.cpp
@@ -17,3 +17,63 @@ int main() {
     
     return 0;
 }
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct NhanVien {
+    string maNV;
+    string tenNV;
+    double luongCoBan;
+    double heSo;
+};
+
+// Ham nhap thong tin 1 nhan vien
+void nhap1NV(NhanVien &nv) {
+    cout << "Nhap ma NV: ";
+    getline(cin, nv.maNV);
+    cout << "Nhap ten NV: ";
+    getline(cin, nv.tenNV);
+    cout << "Nhap luong co ban: ";
+    cin >> nv.luongCoBan;
+    cout << "Nhap he so luong: ";
+    cin >> nv.heSo;
+    cin.ignore(); // Xoa bo nho dem sau khi nhap so
+}
+
+// Ham xuat thong tin 1 nhan vien
+void xuat1NV(const NhanVien &nv) {
+    cout << left << setw(12) << nv.maNV 
+         << setw(25) << nv.tenNV 
+         << setw(15) << fixed << setprecision(0) << nv.luongCoBan 
+         << setw(10) << setprecision(2) << nv.heSo << endl;
+}
+
+// Ham nhap danh sach n nhan vien
+void nhapDS(NhanVien ds[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "\n--- Nhap thong tin nhan vien thu " << i + 1 << " ---" << endl;
+        nhap1NV(ds[i]);
+    }
+}
+
+// Ham xuat danh sach n nhan vien
+void xuatDS(NhanVien ds[], int n) {
+    cout << "\n================ DANH SACH NHAN VIEN ================\n";
+    cout << left << setw(12) << "Ma NV" 
+         << setw(25) << "Ho Ten" 
+         << setw(15) << "Luong CB" 
+         << setw(10) << "He So" << endl;
+    cout << "-----------------------------------------------------\n";
+    for (int i = 0; i < n; i++) {
+        xuat1NV(ds[i]);
+    }
+}
+
+int main() {
+    int n;
+    NhanVien ds[100];
+    return 0;
+}
