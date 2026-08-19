4e978ed568029ccc2fcf343389756ca234b7be91
76DCHT22193_20_NguyenDangDuong <nguyendangduong1222007@gmail.com>
2026-08-19 20:32:13 +0700

    BT01-CP1 : Tao cau truc

---
+++ BT01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Dinh nghia cau truc NhanVien
+struct NhanVien {
+    string maNV;
+    string tenNV;
+    double luongCoBan;
+    double heSo;
+};
+
+int main() {
+    int n;
+    // Khai bao mang nhan vien toi da 100 phan tu
+    NhanVien ds[100];
+    
+    return 0;
+}
